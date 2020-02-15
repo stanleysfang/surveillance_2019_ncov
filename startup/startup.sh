f39d6733154d45bda9c3cfac3945f5a480b4bcad
Stanley Fang <stanleysfang92@gmail.com>
2020-02-15 11:46:13 -0800

    cleaned up startup.sh

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-#### Initialization ####
-
-start_time=$(date)
-
-echo -e "\n================================\nRunning         ${0##/*/} $*\n"
+repository="surveillance_2019_ncov"
 
 #### Startup ####
-
-repository="surveillance_2019_ncov"
 
 # Install Anaconda
 wget "https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh"
@@ -22,13 +16,3 @@ export PATH="$HOME/anaconda3/bin:$PATH"
 conda create -y --name surveillance_2019_ncov python=3.7.0 pip
 source activate surveillance_2019_ncov
 pip install -r $HOME/${repository}/production/requirements.txt
-
-#### Run Time ####
-
-end_time=$(date)
-start=$(date -d "${start_time}" +%s)
-end=$(date -d "${end_time}" +%s)
-secs=$(($end-$start))
-
-echo -e "\nScript          ${0##/*/} $*\nStart Time      ${start_time}\nEnd Time        ${end_time}"
-printf 'Run Time        %d day %d hr %d min %d sec\n================================\n' $(($secs/86400)) $(($secs%86400/3600)) $(($secs%3600/60)) $(($secs%60))
