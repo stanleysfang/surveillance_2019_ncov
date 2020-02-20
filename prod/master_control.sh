eed3427caba071ee5fd0218dc82cd93c2d00908b
Stanley Fang <stanleysfang92@gmail.com>
2020-02-20 12:02:29 -0800

    added master_control.sh

---
+++ prod/master_control.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+start_time=$(date)
+
+#### Environment ####
+
+project="stanleysfang"
+
+gs_bucket="stanleysfang"
+repository="surveillance_2019_ncov"
+
+instance_name="stanleysfang"
+zone="us-west1-b"
+
+#### master_control ####
+
+# gcloud compute instances start ${instance_name} --zone ${zone}
+
+command="gsutil -m cp -r gs://${gs_bucket}/${repository} \$HOME/"
+gcloud compute ssh ${instance_name} --zone ${zone} --command "${command}"
+
+# command="bash \$HOME/surveillance_2019_ncov/prod/ts_2019_ncov_master_wrapper.sh 1>\$HOME/surveillance_2019_ncov/log/ts_2019_ncov_master_wrapper.out 2>&1"
+# gcloud compute ssh ${instance_name} --zone ${zone} --command "${command}"
+
+# command="gsutil -m cp -r \$HOME/${repository}/log gs://${gs_bucket}/${repository}/"
+# gcloud compute ssh ${instance_name} --zone ${zone} --command "${command}"
+
+# gcloud compute instances stop ${instance_name} --zone ${zone}
+
+#### Run Time ####
+end_time=$(date)
+start=$(date -d "${start_time}" +%s)
+end=$(date -d "${end_time}" +%s)
+secs=$(($end-$start))
+
+echo -e "\n================================"
+echo -e "Script: ${0##/*/} $*"
+echo -e "\nStart Time: ${start_time}"
+echo -e "End Time: ${end_time}"
+printf 'Run Time: %d day %d hr %d min %d sec\n' $(($secs/86400)) $(($secs%86400/3600)) $(($secs%3600/60)) $(($secs%60))
+echo -e "================================"
