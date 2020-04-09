e8803d7103d9ec78d8e910dc07b1353fd2eb5ff4
Stanley Fang <stanleysfang92@gmail.com>
2020-04-09 11:35:14 -0700

    updated startup

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
 #### Environment ####
-
-project="stanleysfang"
-
-repository="surveillance_2019_ncov"
+repo="surveillance_2019_ncov"
+repo_path=/home/sfang/windows/gitlab/stanleysfang/${repo}/
 
 #### startup ####
+conda remove -y --name ${repo} --all
 
-# Install Anaconda
-wget "https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh"
-bash $HOME/Anaconda3-5.2.0-Linux-x86_64.sh -b
-echo -e "\n# added by Anaconda3 installer\nexport PATH=\"$HOME/anaconda3/bin:\$PATH\"" >> $HOME/.bashrc # this doesn't work with airflow
-rm $HOME/Anaconda3-5.2.0-Linux-x86_64.sh
-export PATH="$HOME/anaconda3/bin:$PATH"
+conda create -y --name ${repo} python=3.7.0 pip
+source activate ${repo}
+pip install -r ${repo_path}/startup/requirements.txt
 
-# Create Conda Environment
-conda create -y --name surveillance_2019_ncov python=3.7.0 pip
-source activate surveillance_2019_ncov
-pip install -r $HOME/${repository}/startup/requirements.txt
+jupyter kernelspec remove -f ${repo}
+python -m ipykernel install --name ${repo} --display-name "Python 3.7.0 (${repo})" --user
