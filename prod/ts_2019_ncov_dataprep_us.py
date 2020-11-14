8fba6b4b0d35b08440e6c374011a30a62e15c84d
Stanley Fang <stanleysfang92@gmail.com>
2020-11-13 16:55:55 -0800

    added print_job_result function; updated queries

--- prod/ts_2019_ncov_dataprep_us.py
+++ prod/ts_2019_ncov_dataprep_us.py
@@ -15,21 +15,25 @@ qr = QueryRunner(client=client)
 loader = Loader(client=client)
 extractor = Extractor(client=client)
 
-max_results = 20
-
 geo='us'
+
+#### Functions ####
+def print_job_result(job, client, max_results=20):
+    job.result()
+    bq_table = client.get_table(job.destination)
+    df = client.list_rows(bq_table, max_results=max_results).to_dataframe()
+    print(bq_table.full_table_id)
+    print(df.head(max_results))
 
 #### Load Data ####
 confirmed = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv')
 deaths = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv')
 
 dt_cols = [datetime.datetime.strptime(dt, '%m/%d/%y').strftime('dt_%Y%m%d') for dt in list(confirmed.columns)[11:]]
-cols = ['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'city', 'province_state', 'country_region', 'latitude', 'longitude', 'combined_key'] + dt_cols
 
-confirmed.columns = cols
-
-cols.insert(11, 'population')
-deaths.columns = cols
+# confirmed
+confirmed_cols = ['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'county', 'province_state', 'country_region', 'latitude', 'longitude', 'combined_key'] + dt_cols
+confirmed.columns = confirmed_cols
 
 confirmed_schema = [
     ('UID', 'INT64'),
@@ -37,7 +41,7 @@ confirmed_schema = [
     ('iso3', 'STRING'),
     ('code3', 'INT64'),
     ('FIPS', 'FLOAT64'),
-    ('city', 'STRING'),
+    ('county', 'STRING'),
     ('province_state', 'STRING'),
     ('country_region', 'STRING'),
     ('latitude', 'FLOAT64'),
@@ -45,10 +49,17 @@ confirmed_schema = [
     ('combined_key', 'STRING'),
 ] + [(dt_col, 'INT64') for dt_col in dt_cols]
 
-confirmed_load_job = loader.load_df(confirmed, 'stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}_confirmed_raw'.format(geo=geo), schema=schema)
+confirmed_load_job = loader.load_df(confirmed, 'stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}_confirmed_raw'.format(geo=geo), schema=confirmed_schema)
 
-schema.insert(11, ('population', 'INT64'))
-deaths_load_job = loader.load_df(deaths, 'stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}_deaths_raw'.format(geo=geo), schema=schema)
+#deaths
+deaths_cols = confirmed_cols
+deaths_cols.insert(11, 'population')
+deaths.columns = deaths_cols
+
+deaths_schema = confirmed_schema
+deaths_schema.insert(11, ('population', 'INT64'))
+
+deaths_load_job = loader.load_df(deaths, 'stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}_deaths_raw'.format(geo=geo), schema=deaths_schema)
 
 for job in loader.job_history:
     job.result()
@@ -60,54 +71,83 @@ for job in loader.job_history:
     print(df.head(max_results))
 
 #### Dataprep ####
-ts_2019_ncov_temp_query = ''
-for dt_col in dt_cols:
-    ts_2019_ncov_temp_query = \
-    """{ts_2019_ncov_temp_query}
-    SELECT
-        PARSE_DATE('dt_%Y%m%d', '{dt_col}') AS dt,
-        COALESCE(a.city, b.city) AS city,
-        COALESCE(a.province_state, b.province_state) AS province_state,
-        COALESCE(a.country_region, b.country_region) AS country_region,
-        COALESCE(a.combined_key, b.combined_key) AS combined_key,
-        COALESCE(a.latitude, b.latitude) AS latitude,
-        COALESCE(a.longitude, b.longitude) AS longitude,
-        IFNULL(a.{dt_col}, 0) AS confirmed,
-        IFNULL(b.{dt_col}, 0) AS deaths,
-        0 AS recovered
-    FROM `stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}_confirmed_raw` a
-    FULL JOIN `stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}_deaths_raw` b
-    ON a.combined_key = b.combined_key
+for metric in ['confirmed', 'deaths']:
+    array_query = ''
+    for dt_col in dt_cols:
+        array_query = \
+        """{array_query}
+        STRUCT(PARSE_DATE('dt_%Y%m%d', '{dt_col}') AS dt, {dt_col} AS {metric}),
+        """.format(array_query=array_query, dt_col=dt_col, metric=metric)
+    array_query = re.sub('[,\n ]*$', '\n', array_query)
     
-    UNION ALL
-    """.format(ts_2019_ncov_temp_query=ts_2019_ncov_temp_query, dt_col=dt_col, geo=geo)
-
-ts_2019_ncov_temp_query = re.sub('[(UNION ALL)\n ]*$', '\n', ts_2019_ncov_temp_query)
+    query = \
+    """
+    SELECT dt, a.* EXCEPT(arr), {metric}
+    FROM (
+        SELECT
+            county, province_state, country_region, combined_key, latitude, longitude,
+            [{array_query}] AS arr
+        FROM `stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}_{metric}_raw`
+    ) a, UNNEST(arr)
+    """.format(metric=metric, array_query=array_query, geo=geo)
+    
+    query_job = qr.run_query(query, destination_table='stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}_{metric}'.format(geo=geo, metric=metric), time_partitioning=True, partition_field='dt')
+    query_job.result()
+    
+    bq_table = client.get_table(query_job.destination)
+    df = client.list_rows(bq_table, max_results=max_results).to_dataframe()
+    
+    print(bq_table.full_table_id)
+    print(df.head(max_results))
 
 ts_2019_ncov_query = \
 """
 SELECT
-    dt, province_state, country_region, latitude, longitude,
-    confirmed, deaths, recovered,
-    IFNULL(confirmed_new, confirmed) AS confirmed_new,
-    IFNULL(deaths_new, deaths) AS deaths_new,
-    recovered_new,
-    TIMESTAMP(REGEXP_REPLACE(STRING(CURRENT_TIMESTAMP, "America/Los_Angeles"), r'[\+-][0-9]{{2}}$', '')) AS last_updated_ts
+    dt, county, province_state, country_region, combined_key, latitude, longitude,
+    population,
+    confirmed, deaths, confirmed_new, deaths_new,
+    ROUND(AVG(confirmed_new) OVER(PARTITION BY combined_key ORDER BY dt ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 1) AS confirmed_new_7d_ma,
+    ROUND(AVG(deaths_new) OVER(PARTITION BY combined_key ORDER BY dt ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 1) AS deaths_new_7d_ma,
+    ROUND(AVG(confirmed_new) OVER(PARTITION BY combined_key ORDER BY dt ROWS BETWEEN 27 PRECEDING AND CURRENT ROW), 1) AS confirmed_new_28d_ma,
+    ROUND(AVG(deaths_new) OVER(PARTITION BY combined_key ORDER BY dt ROWS BETWEEN 27 PRECEDING AND CURRENT ROW), 1) AS deaths_new_28d_ma,
+    IF(population = 0, NULL, ROUND(confirmed/population, 4)) AS incident_rate,
+    IF(confirmed = 0, NULL, ROUND(deaths/confirmed, 4)) AS case_fatality_rate,
+    MAX(dt) OVER() AS last_update_dt,
+    TIMESTAMP(REGEXP_REPLACE(STRING(CURRENT_TIMESTAMP, "America/Los_Angeles"), r'[\+-][0-9]{2}$', '')) AS last_updated_ts
 FROM (
     SELECT
-        dt,
-        CASE
-            WHEN city IS NULL OR city = 'Unassigned' THEN province_state
-            ELSE CONCAT(city, ", ", province_state)
-            END AS province_state,
-        country_region, latitude, longitude,
-        confirmed, deaths, recovered,
-        confirmed - LAG(confirmed) OVER(PARTITION BY combined_key ORDER BY dt) AS confirmed_new,
-        deaths - LAG(deaths) OVER(PARTITION BY combined_key ORDER BY dt) AS deaths_new,
-        0 AS recovered_new
-    FROM ({ts_2019_ncov_temp_query})
+        * EXCEPT(confirmed_new, deaths_new),
+        IFNULL(confirmed_new, confirmed) AS confirmed_new,
+        IFNULL(deaths_new, deaths) AS deaths_new
+    FROM (
+        SELECT
+            *,
+            confirmed - LAG(confirmed) OVER(PARTITION BY combined_key ORDER BY dt) AS confirmed_new,
+            deaths - LAG(deaths) OVER(PARTITION BY combined_key ORDER BY dt) AS deaths_new
+        FROM (
+            SELECT
+                a.*, b.population
+            FROM (
+                SELECT
+                    COALESCE(a.dt, b.dt) AS dt,
+                    COALESCE(a.county, b.county) AS county,
+                    COALESCE(a.province_state, b.province_state) AS province_state,
+                    COALESCE(a.country_region, b.country_region) AS country_region,
+                    COALESCE(a.combined_key, b.combined_key) AS combined_key,
+                    COALESCE(a.latitude, b.latitude) AS latitude,
+                    COALESCE(a.longitude, b.longitude) AS longitude,
+                    IFNULL(a.confirmed, 0) AS confirmed,
+                    IFNULL(b.deaths, 0) AS deaths
+                FROM `stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}_confirmed` a
+                FULL JOIN `stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}_deaths` b
+                ON a.dt = b.dt AND a.combined_key = b.combined_key
+            ) a
+            LEFT JOIN `stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}_deaths_raw` b
+            ON a.combined_key = b.combined_key
+        )
+    )
 )
-""".format(ts_2019_ncov_temp_query=ts_2019_ncov_temp_query)
+""".format(geo=geo)
 
 query_job = qr.run_query(ts_2019_ncov_query, destination_table='stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}'.format(geo=geo), time_partitioning=True, partition_field='dt')
 query_job.result()
@@ -118,8 +158,15 @@ df = client.list_rows(bq_table, max_results=max_results).to_dataframe()
 print(bq_table.full_table_id)
 print(df.head(max_results))
 
+us_cur_query = \
+"""
+SELECT *
+FROM `stanleysfang.surveillance_2019_ncov.ts_2019_ncov_{geo}`
+WHERE dt = last_update_dt
+""".format(geo=geo)
+
 #### Extract Table ####
-for dt_col in dt_cols[-7:]: #  only refresh the last 7 days
+for dt_col in dt_cols[-7:]: #  only refresh the last 14 days
     dt = datetime.datetime.strptime(dt_col, 'dt_%Y%m%d').strftime('%Y-%m-%d')
     print(dt)
     
